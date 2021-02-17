c7c9d5ab617cd0730a1e9499aecba1840be88bd6
spewil <spencer@spewil.com>
2021-02-17 15:21:47 +0000

    Add data analysis tools

--- utils/utils.py
+++ utils/utils.py
@@ -69,3 +69,22 @@ def align_trajectory(trajectory, reference):
         return trajectory
     else:
         return -trajectory
+
+
+def random_matrices():
+    # A problem with a low condition number is said to be well-conditioned, while a problem with a high condition number is said to be ill-conditioned.
+
+    size = 5
+    conds = []
+    for _ in range(1000):
+        A = np.random.normal(size=(size, size))
+        # print(A)
+        # print(np.linalg.norm(A, axis=0))
+        A_normed = A / np.linalg.norm(A, axis=0)
+        # print(A_normed)
+        # print(np.sqrt(np.sum(A_normed[:, 1]**2)))
+        conds.append(np.linalg.cond(A_normed))
+
+    print(np.min(conds), np.max(conds), np.median(conds), np.mean(conds))
+    # plt.hist(conds, bins=100)
+    # plt.show()
